0e3e8282c87c096ba913c08d95d7575ebaeb9d1c
Alex Guo <alexguo117@gmail.com>
2021-03-08 13:14:54 -0800

    adding storybook actions

--- components/.storybook/main.js
+++ components/.storybook/main.js
@@ -1,22 +1,21 @@
-const path = require('path');module.exports = {
-    stories: ['../src/components/**/*.stories.[tj]s'],
-    addons: ['@storybook/addon-knobs/register'],
+const path = require("path");
+module.exports = {
+    stories: ["../src/components/**/*.stories.[tj]s"],
+    addons: ["@storybook/addon-actions", "@storybook/addon-knobs/register"],
     webpackFinal: async (config, { configType }) => {
-    
         config.module.rules.push({
             test: /\.(ts|tsx)$/,
-            loaders: ['babel-loader', 'ts-loader']
+            loaders: ["babel-loader", "ts-loader"],
         });
-    
+
         config.module.rules.push({
             test: /\.scss$/,
-            loaders: ['style-loader', 'css-loader', 'sass-loader']
+            loaders: ["style-loader", "css-loader", "sass-loader"],
         });
-    
-        config.resolve.extensions.push('.ts', '.tsx', ".scss");
-    
+
+        config.resolve.extensions.push(".ts", ".tsx", ".scss");
+
         // Return the altered config
         return config;
-    }
-}
-    
+    },
+};
--- components/src/components/Buttons.stories.js
+++ components/src/components/Buttons.stories.js
@@ -1,10 +1,39 @@
-import * as React from 'react'
-import { HighlightedButton } from './Buttons'
-import { withKnobs, text } from '@storybook/addon-knobs'
+import * as React from "react";
+import { HighlightedButton } from "./Buttons";
+import { withKnobs, text } from "@storybook/addon-knobs";
 
-export default { title: 'Highlighted Button', decorators: [withKnobs]}
+// export default {
+//     title: "Highlighted Button",
+//     argTypes: { action: { action: "click" } },
+//     decorators: [withKnobs],
+// };
 
-export const highlighted_button = () => {
-  const content = text('Text', 'This is a button')
-  return <HighlightedButton>{content}</HighlightedButton>
-}
+// export const highlighted_button = () => {
+//     const content = text("Text", "This is a button");
+//     return <HighlightedButton>{content}</HighlightedButton>;
+// };
+
+// export const highlighted_working_button = () => {
+//     const content = text("Text", "Go to Google");
+//     const actionFunc = () => {
+//         console.log("yay!");
+//     };
+//     const href = "https://www.google.com/";
+//     return (
+//         <HighlightedButton id={"google_btn"} action={actionFunc}>
+//             {content}
+//         </HighlightedButton>
+//     );
+// };
+
+export default {
+    title: "Highlighted Button",
+    decorators: [withKnobs],
+};
+
+const Template = (args) => <HighlightedButton {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    action: () => console.log("yeet"),
+};
