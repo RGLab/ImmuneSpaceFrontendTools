699945573d39f5b72c3796775f78918f62aa56d5
Alex Guo <alexguo117@gmail.com>
2021-03-10 16:01:32 -0800

    added bootstrap

---
+++ components/src/components/Dropdowns.stories.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from "@storybook/react";
+import {
+    ButtonData,
+    DropdownButtonProps,
+    DropdownContentProps,
+    OuterDropdownButtonProps,
+    SimpleDropdown,
+} from "./Dropdowns";
+
+export default {
+    title: "Dropdowns",
+    component: SimpleDropdown,
+};
+
+const SimpleTemplate: Story<DropdownButtonProps> = (args) => <SimpleDropdown {...args} />;
+export const Primary = SimpleTemplate.bind({});
+
+const btnData: ButtonData = {
+    label: "data",
+    icon: <div>Hello</div>,
+    action: () => {},
+    disabled: false,
+    href: "#",
+};
+
+Primary.args = {
+    title: "Drop Down",
+    buttonData: [btnData, btnData],
+    disabled: false,
+    id: "12345",
+};
