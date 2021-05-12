429d06a40ee48b6d7018ee2a0ddcdcff24c0d9b4
Alex Guo <59902470+guoz24@users.noreply.github.com>
2021-05-11 18:12:54 -0700

    Dev to main (#8)
    * npm update
    
    * upgraded storybook to version 6.1.21 (#6)
    
    Co-authored-by: Alex Guo <alexguo117@gmail.com>
    
    * integrate cognition styling (#7)
    
    * refactored entrance page styling
    
    * versioned
    
    * updated webpack and versioning
    
    Co-authored-by: Alex Guo <alexguo117@gmail.com>
    
    * version bump
    
    Co-authored-by: Alex Guo <alexguo117@gmail.com>

--- theme/webpack.config.js
+++ theme/webpack.config.js
@@ -1,25 +1,41 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: {},
-    mode: 'production',
+    mode: "production",
     plugins: [
         // // add a hash to the filename of our bundles for easy and efficient cache busting.
         // new MiniCssExtractPlugin({
         //     filename: isDevelopment ? '[name].css' : '[name].[hash].css',
         //     chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
         // })
-        
-        // copy over variables and let downstream modules compile 
+
+        // copy over variables and let downstream modules compile
         new CopyWebpackPlugin({
             patterns: [
                 {
                     // copy static scss files into the dist dir to be used by LabKey module apps
-                    from: '_immunespace.scss',
-                    to: ''
-                }
-            ]
+                    from: "_immunespace.scss",
+                    to: "",
+                },
+                {
+                    from: "_immunespace.fonts.scss",
+                    to: "",
+                },
+                {
+                    from: "_immunespace.mixins.scss",
+                    to: "",
+                },
+                {
+                    from: "_immunespace.responsive.scss",
+                    to: "",
+                },
+                {
+                    from: "_immunespace.typography.scss",
+                    to: "",
+                },
+            ],
         }),
-    ]
+    ],
 };
