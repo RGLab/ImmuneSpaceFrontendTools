112c13f9a66d86a964eeba4f73c52c6c1dd49a5a
Helen Miller <hmiller@fredhutch.org>
2021-11-04 15:15:14 -0700

    Components: No read header

--- components/src/components/Greeter.tsx
+++ components/src/components/Greeter.tsx
@@ -6,6 +6,6 @@ interface GreeterProps {
     name: string;
 }
 
-const Greeter: React.FC<GreeterProps> = ({ name }) => <h1>Hello {name}</h1>;
+const Greeter: React.FC<GreeterProps> = ({ name }) => <h1 className="greeter">Hello {name}</h1>;
 
 export default Greeter;
